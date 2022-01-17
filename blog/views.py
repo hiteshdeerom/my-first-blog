7b19f83a8d5061793332995e557a6b2410fbceb6
hiteshdeerom <hitesh@deerom.com>
2022-01-17 15:11:22 +0530

    added a comment and like section

--- blog/views.py
+++ blog/views.py
@@ -2,19 +2,22 @@ from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
-from .froms import PostForm
+from .froms import PostForm,CommentForm
 from django.shortcuts import redirect
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')  
     return render(request, 'blog/landing.html', {'posts':posts}) 
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
 def post_new(request):
     form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -40,3 +43,18 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form}) 
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+
